b8b5427a7be85637ecbcb8f2a9374236c1b0f2cd
Evgeniy Kvasov <E_kvasov@inbox.ru>
2022-08-17 15:36:25 +0500

    Нерешенные вопросы

---
+++ async-requests/poll/task.js
@@ -0,0 +1,24 @@
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
+xhr.send();
+
+xhr.addEventListener("readystatechange", () => {
+  if(xhr.readyState === xhr.DONE) {
+    const text = JSON.parse(xhr.responseText);
+    const pollTitle = document.getElementById("poll__title");
+    pollTitle.innerHTML = `${text.data.title}`;
+    const answers = text.data.answers;
+    const pollAnswers = document.getElementById("poll__answers");
+    answers.forEach(element => {
+      pollAnswers.insertAdjacentHTML('beforeEnd', `<button class="poll__answer">
+                                                    ${element}
+                                                  </button>`);
+    })
+  }
+})
+
+const pollAnswer = Array.from(document.querySelectorAll(".poll__answer"));
+console.log(pollAnswer);
+pollAnswer.forEach(element => {
+  element.onclick = () => console.log("Спасибо, ваш голос засчитан");
+})
